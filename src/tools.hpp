d7bde7c8154411cbdc72ece02d6e0569b94af1ee
Philipp Lutz <philipp.lutz@gmx.de>
2018-04-28 16:53:04 +0200

    Added efficient implementation of moving average filter

--- src/tools.hpp
+++ src/tools.hpp
@@ -1,3 +1,6 @@
+#ifndef _TOOLS_HPP
+#define _TOOLS_HPP
+
 #include <vector>
 
 using namespace std;
@@ -27,3 +30,36 @@ void Tranform2d(std::vector<double>       &dst_x,
                 const std::vector<double> &src_x,
                 const std::vector<double> &src_y,
                 double translation_x, double translation_y, double psi);
+
+
+// Efficient moving average implementation, 
+// avoiding costly O(N) traversal to calculate the total sum.
+// https://stackoverflow.com/questions/10990618/calculate-rolling-moving-average-in-c#10990893
+template <typename T, typename Total, int N>
+class Moving_Average
+{
+  public:
+    Moving_Average() : num_samples_(0), total_(0) { }
+
+    void operator()(T sample) {
+        if (num_samples_ < N) {
+            samples_[num_samples_++] = sample;
+            total_ += sample;
+        } else {
+            T& oldest = samples_[num_samples_++ % N];
+            total_ += sample - oldest;
+            oldest = sample;
+        }
+    }
+
+    operator T() const {
+    	return total_ / std::min(num_samples_, N);
+    }
+
+  private:
+    T samples_[N];
+    int num_samples_;
+    Total total_;
+};
+
+#endif  // _TOOLS_HPP
